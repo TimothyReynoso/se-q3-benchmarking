56d91f39c5781d5a8b5380fcd95a1e3b1db4e383
Timothy Reynoso <domex2467@gmail.com>
2020-10-06 16:30:03 -0400

    completed project

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,7 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Timothy Reynoso"
 
 import sys
 
@@ -27,11 +27,18 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    # anagrams = {
+    #     alphabetize(word): [
+    #         w for w in words
+    #         if alphabetize(w) == alphabetize(word)]
+    #     for word in words}
+
+    anagrams = {}
+    for w in words:
+        anagrams.setdefault(alphabetize(w), [w])
+        if w not in anagrams[alphabetize(w)]:
+            anagrams[alphabetize(w)].append(w)
+    print(anagrams)
     return anagrams
 
 
